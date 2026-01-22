1a036e6c1aba137ff47443e1c704baf0eddb3ee3
Hanmi <hanxu7261@gmail.com>
2026-01-22 19:21:07 +0800

    feat: init lazyvim config

---
+++ lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+---@diagnostic disable-next-line: undefined-global
+vim.g.python3_host_prog = vim.fn.expand("~/.config/nvim/py3env/bin/python")
